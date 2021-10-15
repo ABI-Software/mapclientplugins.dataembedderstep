d80d208c2987527141e2e887520f435a56bbad0f
Richard Christie <r.christie@auckland.ac.nz>
2021-10-15 13:25:51 +1300

    Initial version

---
+++ mapclientplugins/dataembedderstep/__init__.py
@@ -0,0 +1,13 @@
+
+"""
+MAP Client Plugin
+"""
+
+__version__ = '0.1.0'
+__author__ = 'Richard Christie'
+__stepname__ = 'DataEmbedder'
+__location__ = ''
+
+# import class that derives itself from the step mountpoint.
+from mapclientplugins.dataembedderstep import step
+
