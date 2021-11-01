d158b1ba1ea3ac6d24733053edf142298f56dc90
hsorby <h.sorby@auckland.ac.nz>
2021-11-02 00:12:42 +1300

    Set the location of the step in the metadata.

--- mapclientplugins/dataembedderstep/__init__.py
+++ mapclientplugins/dataembedderstep/__init__.py
@@ -6,7 +6,7 @@ MAP Client Plugin
 __version__ = '0.1.0'
 __author__ = 'Richard Christie'
 __stepname__ = 'DataEmbedder'
-__location__ = ''
+__location__ = 'https://github.com/ABI-Software/mapclientplugins.dataembedderstep'
 
 # import class that derives itself from the step mountpoint.
 from mapclientplugins.dataembedderstep import step
