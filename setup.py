c03d4c924df457411dda7f28724629285b7bfc0f
hsorby <h.sorby@auckland.ac.nz>
2021-11-04 17:25:29 +1300

    Use single source version in setup.py.

--- setup.py
+++ setup.py
@@ -1,26 +1,40 @@
-from setuptools import setup, find_packages
-from setuptools.command.install import install
-import os
+import codecs
 import io
+import os
+import re
+
+from setuptools import setup, find_packages
 
 SETUP_DIR = os.path.dirname(os.path.abspath(__file__))
 
+
+def read(*parts):
+    with codecs.open(os.path.join(SETUP_DIR, *parts), 'r') as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
 # List all of your Python package dependencies in the
 # requirements.txt file
-
-
 def readfile(filename, split=False):
     with io.open(filename, encoding="utf-8") as stream:
         if split:
             return stream.read().split("\n")
         return stream.read()
 
+
 readme = readfile("README.rst", split=True)[3:]  # skip title
-# For requirements not hosted on PyPi place listings
-# into the 'requirements.txt' file.
 requires = [
     # minimal requirements listing
-    "dataembedder @ https://api.github.com/repos/ABI-Software/dataembedder/tarball/main",
+    "dataembedder @ https://api.github.com/repos/ABI-Software/dataembedder/tarball/v0.1.0",
     "opencmiss.maths",
     "opencmiss.utils >= 0.3",
     "opencmiss.zinc >= 3.5",
@@ -29,32 +43,22 @@ requires = [
 ]
 source_license = readfile("LICENSE")
 
-
-class InstallCommand(install):
-
-    def run(self):
-        install.run(self)
-        # Automatically install requirements from requirements.txt
-        import subprocess
-        subprocess.call(['pip', 'install', '-r', os.path.join(SETUP_DIR, 'requirements.txt')])
-
-
-setup(name='mapclientplugins.dataembedderstep',
-    version='0.1.0',
+setup(
+    name='mapclientplugins.dataembedderstep',
+    version=find_version('mapclientplugins', 'dataembedderstep', '__init__.py'),
     description='',
     long_description='\n'.join(readme) + source_license,
     classifiers=[
-      "Development Status :: 3 - Alpha",
-      "License :: OSI Approved :: Apache Software License",
-      "Programming Language :: Python",
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
     ],
-    cmdclass={'install': InstallCommand,},
     author='Richard Christie',
     author_email='',
     url='https://github.com/ABI-Software/mapclientplugins.dataembedderstep',
-    packages=find_packages(exclude=['ez_setup',]),
+    packages=find_packages(exclude=['ez_setup', ]),
     namespace_packages=['mapclientplugins'],
     include_package_data=True,
     zip_safe=False,
     install_requires=requires,
-    )
+)
