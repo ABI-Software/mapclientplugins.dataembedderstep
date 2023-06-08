ffc1806ddb965f4a7bf0bd2fb1800a9100638705
Richard Christie <r.christie@auckland.ac.nz>
2023-06-08 23:22:29 +1200

    Use common utils in cmlibs.utils

--- mapclientplugins/dataembedderstep/model/dataembeddermodel.py
+++ mapclientplugins/dataembedderstep/model/dataembeddermodel.py
@@ -6,13 +6,13 @@ import json
 
 from cmlibs.utils.zinc.finiteelement import evaluate_field_nodeset_range
 from cmlibs.utils.zinc.general import ChangeManager
+from cmlibs.utils.zinc.group import group_add_group_elements, group_add_group_nodes
+from cmlibs.utils.zinc.scene import scene_get_selection_group, scene_create_selection_group
 from cmlibs.zinc.field import Field, FieldGroup
 from cmlibs.zinc.glyph import Glyph
 from cmlibs.zinc.graphics import Graphics
 from cmlibs.zinc.material import Material
 from dataembedder.dataembedder import DataEmbedder
-from mapclientplugins.dataembedderstep.utils.zinc_utils import get_scene_selection_group, create_scene_selection_group,\
-    group_add_group_elements, group_add_group_nodes
 
 
 nodeDerivativeLabels = ["D1", "D2", "D3", "D12", "D13", "D23", "D123"]
@@ -401,18 +401,17 @@ class DataEmbedderModel(object):
         with ChangeManager(fieldmodule):
             scene = region.getScene()
             # can't use SUBELEMENT_HANDLING_MODE_FULL as some groups have been tweaked to omit some faces
-            selectionGroup = get_scene_selection_group(
-                scene, subelement_handling_mode=FieldGroup.SUBELEMENT_HANDLING_MODE_NONE)
+            selectionGroup = scene_get_selection_group(scene)
             if group:
                 if selectionGroup:
                     selectionGroup.clear()
                 else:
-                    selectionGroup = create_scene_selection_group(
-                        scene, subelement_handling_mode=FieldGroup.SUBELEMENT_HANDLING_MODE_NONE)
+                    selectionGroup = scene_create_selection_group(scene)
+                oldSubelementHandlingMode = selectionGroup.getSubelementHandlingMode()
+                selectionGroup.setSubelementHandlingMode(FieldGroup.SUBELEMENT_HANDLING_MODE_NONE)
                 group_add_group_elements(selectionGroup, group, highest_dimension_only=False)
-                for fieldDomainType in (Field.DOMAIN_TYPE_NODES, Field.DOMAIN_TYPE_DATAPOINTS):
-                    group_add_group_nodes(selectionGroup, group,
-                                          fieldmodule.findNodesetByFieldDomainType(fieldDomainType))
+                group_add_group_nodes(selectionGroup, group, Field.DOMAIN_TYPE_DATAPOINTS)
+                selectionGroup.setSubelementHandlingMode(oldSubelementHandlingMode)
             else:
                 if selectionGroup:
                     selectionGroup.clear()
